4b1c48bab5aafc40d6be76e7a7f11d2305c25eb6
Marcin Majkowski <pl.marcin.m@gmail.com>
2017-07-23 10:31:21 +0200

    Fix failing AppComponent test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { CustomMaterialModule } from './custom-material/custom-material.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        CustomMaterialModule
+      ]
     }).compileComponents();
   }));
 
