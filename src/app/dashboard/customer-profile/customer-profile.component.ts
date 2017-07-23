5cfcdb9dd8ed7268a6c9eb1024ab3c55945aeaf3
Marcin Majkowski <pl.marcin.m@gmail.com>
2017-07-23 18:20:02 +0200

    Simplify customer profile component

--- src/app/dashboard/customer-profile/customer-profile.component.ts
+++ src/app/dashboard/customer-profile/customer-profile.component.ts
@@ -1,33 +1,25 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-import { ActivatedRoute, Data, ParamMap } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Customer } from '../../customer/customer.model';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-customer-profile',
   templateUrl: './customer-profile.component.html'
 })
-export class CustomerProfileComponent implements OnInit, OnDestroy {
+export class CustomerProfileComponent implements OnInit {
 
-  private subscriptions: Subscription[] = [];
+  isEdit$: Observable<boolean>;
 
-  isEdit = false;
-
-  customer: Customer;
+  customer$: Observable<Customer>;
 
   constructor(
     private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    this.customer = this.route.snapshot.data['customer'];
-    this.subscriptions.push(this.route.data
-      .subscribe((data: Data) => this.customer = data['customer']));
-    this.subscriptions.push(this.route.queryParamMap
-      .subscribe((paramMap: ParamMap) => this.isEdit = paramMap.has('edit')));
+    this.customer$ = this.route.data.map(data => data['customer']);
+    this.isEdit$ = this.route.queryParamMap.map(paramMap => paramMap.has('edit'));
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(subscription => subscription.unsubscribe());
-  }
 }
